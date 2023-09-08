9ef24c45289826a026b1f9a9e4168f9d87cadca2
Thiago Zago <thiagoszago@hotmail.com>
2023-09-08 15:05:07 -0300

    medidas: lista estruturas sequenciais - Lógica de Programação

---
+++ ex-sequenciais-em-java/medidas.java
@@ -0,0 +1,33 @@
+package cursoLogicaProgramacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double A, B, C;
+		double tri, qua, tra;
+		
+		System.out.print("Digite a medida A: ");
+		A = sc.nextDouble();
+		System.out.print("Digite a medida B: ");
+		B = sc.nextDouble();
+		System.out.print("Digite a medida C: ");
+		C = sc.nextDouble();
+		
+		qua = A*A;
+		tri = A*B/2;
+		tra = (A+B)*C/2;
+		
+		System.out.println("AREA DO QUADRADO = " + String.format("%.4f", qua));
+		System.out.println("AREA DO TRIANGULO = " + String.format("%.4f", tri));
+		System.out.println("AREA DO TRAPEZIO = " + String.format("%.4f", tra));
+		
+		sc.close();
+	}
+}

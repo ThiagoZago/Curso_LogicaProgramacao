fa1809a1c24bbc03cfd529a8bf8df3878e7379e4
Thiago Zago <thiagoszago@hotmail.com>
2023-09-08 14:04:18 -0300

    terreno: lista estruturas sequenciais - Lógica de Programação

---
+++ ex-sequenciais-em-java/terreno.java
@@ -0,0 +1,30 @@
+package cursoLogicaProgramacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double larg, comp, valorMetro, area, preco;
+		
+		System.out.print("Digite a largura do terreno: ");
+		larg = sc.nextDouble();
+		System.out.print("Digite o comprimento do terreno: ");
+		comp = sc.nextDouble();
+		System.out.print("Digite o valor do metro quadrado: ");
+		valorMetro = sc.nextDouble();
+		
+		area = comp * larg;
+		preco = area * valorMetro;
+		
+		System.out.println("Area do terreno = " + String.format("%.2f", area));
+		System.out.println("Preco do terreno = " + String.format("%.2f", preco));
+		
+		sc.close();
+	}
+}
